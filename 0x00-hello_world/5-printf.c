6ff1cb253d12dcfb1f8ec6848ca8d88ff578b5f5
Alsaahir <jacobwilsonpmwale@gmai.com>
2022-03-11 10:55:18 -0800

    commit for 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * printf - prints the text in quotes
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a peice of art,");
+	return(0);
+}
