e396238fdf7b3fb40a75db0b262750beb715203c
Alsaahir <jacobwilsonpmwale@gmai.com>
2022-03-11 11:53:22 -0800

    bugfix 2 for 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-/*
- * main - Entry point
- * printf - prints the text in quotes
- */
-
+/*printf - prints the text in quotes */
 int main()
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
