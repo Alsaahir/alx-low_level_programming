147e668608887e2d90fb57d6f01f6512fef58c97
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-11 03:03:17 -1200

    correction for 5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+	/*the puts keyword prints the text in the quotes to the 
+	 * standard output
+	 */
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+	/* the printf statement prints the text in the quotes
+	 * to the standard output
+	 */
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
 }
