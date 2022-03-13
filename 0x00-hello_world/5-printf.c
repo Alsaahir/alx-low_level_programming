57cc079fffdca2ba873e5bb3826a6ebe867373cd
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-12 12:59:31 -1200

    coding style fix for 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /** 
  * printf prints the text in the quotes to the standard output
  * return - returns 0 (Succees)
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
