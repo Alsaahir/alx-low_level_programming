e80a1f2d6e44cc14a6fc825000310dccf50c50eb
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-12 13:33:31 -1200

    correction for 101-quotes.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - prints exactly "and that piece of art is useful
  * Dora Korpar, 2015-10-19",
+ *
  * followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * Return 1: returns (Not successful)
  */
 int main(void)
 {
