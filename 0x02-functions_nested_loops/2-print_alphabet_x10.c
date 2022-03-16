c664b44bd51c90dcc1554ed9adb52a8e344a0984
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-15 22:36:05 -1200

    correction print_alphabet_x10(void);

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * print_alphabet_x10 - Entry point
+ * Description: Prints the alphabet 10 times
+ * Return: Always 0.
+ */
 void print_alphabet_x10(void)
 {
 	int i = 1;
