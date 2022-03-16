2ea8e7349668403100720b1d3daee2650f5fe6b2
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-15 22:05:25 -1200

    commit for 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description: Print the Alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
