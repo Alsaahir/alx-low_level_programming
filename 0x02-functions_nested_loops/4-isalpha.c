2e6e7cc62afceec47c4cce5cd69a6806fedc21be
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-15 23:25:55 -1200

    commit for 4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * Description: checks if character is alphabet
+ * @c: contains character to be evaluated
+ * return: always 0.
+ */
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)));
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
