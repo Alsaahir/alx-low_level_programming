7ff4f9ead28443f40eb35388cdae2243be69ba17
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-15 22:33:32 -1200

    commit for void print_alphabet_x10(void);

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+	while (i <= 1)
+	{
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
+	}
+{
