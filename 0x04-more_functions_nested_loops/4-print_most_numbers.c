0bdcf5f3d65ded430e8852566cfcaf64ec4aa566
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-17 18:57:55 -1200

    commit for 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - display all number from 0 to 9 followed by new line.
+ *
+ * Return: 0 void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if ((i != 50) && (i != 52))
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
