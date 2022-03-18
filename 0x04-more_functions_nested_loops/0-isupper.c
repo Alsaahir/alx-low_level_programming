d31245437ff3c8d604d7e95902a841707817ace9
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-17 17:44:23 -1200

    correction for 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
+/**
+ * _isupper - Check if uppercase
+ *
+ * return: 1 if true
+ * and 0 if false
+ */
 int _isupper(int c)
 {
-    if (c >= 65 && c <= 90)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
