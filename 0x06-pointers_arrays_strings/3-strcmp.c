04d1236cf69382ea9c906fd3f430c279a2cbb79d
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-23 22:48:34 -1200

    reverse array

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcmp - compare 2 string
  * @s1:string
@@ -12,12 +13,10 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-
 	if (*s1 == *s2)
 	{
 		return (0);
 	}
-
 	else
 	{
 		return (*s1 - *s2);
