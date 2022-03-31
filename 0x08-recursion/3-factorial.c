b182f785c4eb44a21ed7480c396ddcfc59f8a7d4
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-03-30 23:56:42 -1200

    bugfix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -9,17 +9,17 @@ int factorial(int n)
 {
 	int x;
 
-	if (n < 0)
-	{
-		return (-1);
-	}
-	else if ((n == 0) || (n == 1))
+	if (n == 0)
 	{
 		return (1);
 	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
 	else
 	{
-		x = factorial(n - 1);
+		x = n * factorial(n - 1);
 	}
 	return (x);
 }
