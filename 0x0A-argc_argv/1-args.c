40b5120436933d79b0843cad65565c9edbfbc1ad
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-04-01 23:04:39 -1200

    correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,14 +8,9 @@
  */
 int main(int argc, char *argv[]__attribute__((unused)))
 {
-	int i;
-
 	if (argc > 1)
 	{
-		for (i = 0; i < argc; i++)
-		{
-			printf("%d\n", argc);
-		}
+		printf("%d\n", argc - 1);
 	}
 	return (0);
 }
