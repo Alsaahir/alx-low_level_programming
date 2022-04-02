5a9e5e6642aefb040a0d41a867f40f9f463e4094
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-04-01 22:53:34 -1200

    print number of args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - display num of args
+ * @argc:int
+ * @argv:string
+ * Return:int
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			printf("%d\n", argc)
+		}
+	}
+	return (0);
+}
