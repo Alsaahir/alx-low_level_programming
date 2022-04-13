f5b34157afac2a1d56e2023313f3d0777768ad1c
Alsaahir <jacobwilsonpmwale@gmail.com>
2022-04-13 15:33:50 -0700

    search int

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef _FUNCTION_FILE_H_
+#define _FUNCTION_FILE_H_
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -7,3 +9,4 @@ int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
+#endif
